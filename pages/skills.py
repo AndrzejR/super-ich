b4232e3b03b1e00e0ace19626cfc89c27d9edcd2
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2023-11-04 18:59:49 +0100

    Skills WIP

--- pages/skills.py
+++ pages/skills.py
@@ -1,3 +1,30 @@
 import streamlit as st
+import sqlite3
 
-st.title("Skills - WIP")
+st.title("My Skills")
+
+con = sqlite3.connect("skills.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE skill(name, year, value)")
+
+cur.execute("""
+    INSERT INTO skill VALUES
+        ('AWS', 2023, 120),
+        ('AWS', 2022, 162),
+        ('AWS', 2021, 51),
+        ('Python', 2023, 32),
+        ('Python', 2022, 5),
+        ('German', 2023, 142)
+""")
+con.commit()
+
+res = cur.execute("SELECT * FROM skill")
+list_of_skills = res.fetchall()
+
+col1, col2 = st.columns(2)
+
+for skill in list_of_skills:
+    with col1:
+        st.text(f"{skill}")
+    with col2:
+        st.text(f"{str(value)}")
